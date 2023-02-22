1d4ed5866eb666f287789abeab5cd38bdaed8212
Georgiy Larenko <goshalar@gmail.com>
2023-02-22 15:37:40 +0500

    Add task02 solution

--- Hwk/Hwk03/task02.java
+++ Hwk/Hwk03/task02.java
@@ -1,8 +1,38 @@
 package Hwk.Hwk03;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 // Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка.
 
 public class task02 {
-    
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList <Integer> myList = new ArrayList<>(10);
+
+        for (int i = 0; i < 10; i++) {
+            myList.add(rnd.nextInt(100));
+        }
+
+        System.out.println("Current list: ");
+        System.out.println(myList.toString());
+
+        int max = Collections.max(myList);
+        int min = Collections.min(myList);
+        
+        System.out.println("Max value: " + max + "\nMin value: " + min);
+        System.out.println("Average: " + average(myList));
+
+    }
+
+    static double average (ArrayList<Integer> list){
+        double sum = 0;     
+        for(int i = 0; i < list.size(); i++) {
+            sum += list.get(i);
+        }
+        
+        return sum/list.size();
+    }
     
 }
