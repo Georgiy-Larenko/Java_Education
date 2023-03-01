6528ad8d8c16a367c6b88d781be712f85f3a2aa2
Georgiy Larenko <goshalar@gmail.com>
2023-03-01 21:32:19 +0500

    add task01

--- Hwk/Hwk05/task01.java
+++ Hwk/Hwk05/task01.java
@@ -1,10 +1,8 @@
 package Hwk.Hwk05;
 
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
 
 /*
  * Написать метод, определяющую правильность расстановки скобок в выражении.
@@ -18,89 +16,75 @@ import java.util.TreeMap;
 
 public class task01 {
 
-    // public static void main(String[] args) {
-
-    //     HashMap<String, Integer> openBr = new HashMap<String, Integer>() {
-    //         {
-    //             put("{", 0);
-    //             put("[", 1);
-    //             put("(", 2);
-    //         }
-    //     };
-    //     HashMap<String, Integer> closeBr = new HashMap<String, Integer>() {
-    //         {
-    //             put("}", 0);
-    //             put("]", 1);
-    //             put(")", 2);
-    //         }
-    //     };
-
-
-    //     String text = "[,],{,},(,(,<,>,),)";
-    //     boolean validate = validate(openBr, closeBr, text);
-
-    //     if (validate) {
-    //         System.out.println("Скобки расставлены правильно.");
-    //     } else {
-    //         System.out.println("Скобки расставлены не правильно.");
-    //     }
-
-
-        
-    // }
-
-    // public static boolean validate(HashMap<String, Integer> openBr,
-    //         HashMap<String, Integer> closeBr,
-    //         String text) {
-
-    //     String[] strings = text.split(",");
-
-    //     Deque<String> stack = new ArrayDeque<>();
-
-    //     for (String inLine : strings) {
-
-    //         for (int i = 0; i < inLine.length(); i++) {
-
-    //             char character = inLine.charAt(i);
-    //             String symbol = Character.toString(character);
-
-    //             if (openBr.containsKey(symbol)) {
-    //                 stack.push(symbol);
-
-    //             } else if (closeBr.containsKey(symbol)) {
-
-    //                 if (!stack.isEmpty()) {
-
-    //                     Integer ordinalNumberForCheckedSymbol = closeBr.get(symbol);
-
-    //                     String firstElemFromStack = stack.pop();
-    //                     Integer ordinalLastAddedNumberElemFromStack = openBr.get(firstElemFromStack);
-    //                     boolean isPairTheBrackets = ordinalLastAddedNumberElemFromStack.equals(ordinalNumberForCheckedSymbol);
-
-    //                     if (isPairTheBrackets)
-    //                         break;
-    //                 }
-
-    //                 return false;
-    //             }
-    //         }
-    //     }
-
-    //     return stack.isEmpty();
-    // }
-
     public static void main(String[] args) {
-        validate("[,],{,},(,(,<,>,),)");
-        validate("[,],{,},(,<,),>");
 
-    }
+        HashMap<String, Integer> openBr = new HashMap<String, Integer>() {
+            {
+                put("{", 0);
+                put("[", 1);
+                put("(", 2);
+            }
+        };
+        HashMap<String, Integer> closeBr = new HashMap<String, Integer>() {
+            {
+                put("}", 0);
+                put("]", 1);
+                put(")", 2);
+            }
+        };
 
-    private static void validate(String text) {
-        String[] splitText = text.split(" ");
-        Map <Integer, List <String>> res = new TreeMap<>();
+
+        String text = "[,],{,},(,(,<,>,),)";
+        boolean validate = validate(openBr, closeBr, text);
+
+        if (validate) {
+            System.out.println("Скобки расставлены правильно.");
+        } else {
+            System.out.println("Скобки расставлены не правильно.");
+        }
+
 
         
-
-        
     }
+
+    public static boolean validate(HashMap<String, Integer> openBr,
+            HashMap<String, Integer> closeBr,
+            String text) {
+
+        String[] strings = text.split(",");
+
+        Deque<String> stack = new ArrayDeque<>();
+
+        for (String inLine : strings) {
+
+            for (int i = 0; i < inLine.length(); i++) {
+
+                char character = inLine.charAt(i);
+                String symbol = Character.toString(character);
+
+                if (openBr.containsKey(symbol)) {
+                    stack.push(symbol);
+
+                } else if (closeBr.containsKey(symbol)) {
+
+                    if (!stack.isEmpty()) {
+
+                        Integer ordinalNumberForCheckedSymbol = closeBr.get(symbol);
+
+                        String firstElemFromStack = stack.pop();
+                        Integer ordinalLastAddedNumberElemFromStack = openBr.get(firstElemFromStack);
+                        boolean isPairTheBrackets = ordinalLastAddedNumberElemFromStack.equals(ordinalNumberForCheckedSymbol);
+
+                        if (isPairTheBrackets)
+                            break;
+                    }
+
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
 }
